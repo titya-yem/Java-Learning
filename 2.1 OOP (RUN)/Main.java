feefd50ed723adeaaf07124d52dbe5298dde8c77
Titya Yem <thitya.yem.photo@gmail.com>
2026-03-31 15:22:19 -0400

    Lesson: Reducing Coupling

--- 2.1 OOP (RUN)/Main.java
+++ 2.1 OOP (RUN)/Main.java
@@ -1,10 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
-        var employee = new Employee();
-        employee.setBaseSalary(50_000);
-        employee.setHourlyRate(20);
-        int pay = employee.calculatePay(10);
 
-        System.out.println(pay);
+    public static void main(String[] args) {
+        var browser = new Browser();
+        browser.navigate("google.com");
     }
 }
