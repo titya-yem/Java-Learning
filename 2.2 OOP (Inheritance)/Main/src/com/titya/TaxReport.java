c14f66c594c9c0e9473a8664a9305693dcf32bee
Titya Yem <thitya.yem.photo@gmail.com>
2026-04-06 06:04:05 -0400

    Lesson: Interface (Methord Injection)

--- 2.2 OOP (Inheritance)/Main/src/com/titya/TaxReport.java
+++ 2.2 OOP (Inheritance)/Main/src/com/titya/TaxReport.java
@@ -3,15 +3,15 @@ package com.titya;
 public class TaxReport {
     private TaxCalculator calculator;
 
-    public TaxReport (TaxCalculator calculator) {
-        this.calculator = calculator; // constructor injection
-    }
+//    public TaxReport (TaxCalculator calculator) {
+//        this.calculator = calculator; // constructor injection
+//    }
+//
+//    public void setCalculator(TaxCalculator calculator) {
+//        this.calculator = calculator; // setter injection
+//    }
 
-    public void setCalculator(TaxCalculator calculator) {
-        this.calculator = calculator; // setter injection
-    }
-
-    public void show () {
+    public void show (TaxCalculator calculator) {
         var tax = calculator.calculateTax();
         System.out.println(tax);
     }
