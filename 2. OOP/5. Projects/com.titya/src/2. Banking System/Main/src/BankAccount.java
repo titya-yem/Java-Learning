e7aa68da331fda8fb592058997f1ca32bbf887ff
Titya Yem <thitya.yem.photo@gmail.com>
2026-04-10 14:31:39 -0400

    Finised: Banking System project

--- 2. OOP/5. Projects/com.titya/src/2. Banking System/Main/src/BankAccount.java
+++ 2. OOP/5. Projects/com.titya/src/2. Banking System/Main/src/BankAccount.java
@@ -1,8 +1,9 @@
+import java.util.ArrayList;
+
 public class BankAccount {
     private String name;
     private double balance;
-    private double amount;
-
+    private ArrayList<String> history = new ArrayList<>();
 
     public BankAccount(String name, double balance) {
         this.name = name;
@@ -10,26 +11,40 @@ public class BankAccount {
     }
 
     public void deposit (double amount) {
-        if (amount < 0)
+        if (amount < 0) {
             System.out.println("Invalid amount");
+            return;
+        }
 
         this.balance += amount;
+        history.add("Deposited: " + amount + "$");
         System.out.println("Deposit");
         System.out.println(this.balance);
     }
 
     public void withdraw (double amount) {
-        if (amount < 0 || amount > balance)
+        if (amount < 0 || amount > balance) {
             System.out.println("Invalid amount");
+            return;
+        }
 
         this.balance -= amount;
+        history.add("Withdrawn: " + amount + "$");
         System.out.println("Withdraw");
         System.out.println(this.balance);
     }
 
     public void checkBalance () {
+        if (balance < 0)
+            System.out.println("Invalid balance");
+
         System.out.println("Checking Balance");
         System.out.println("Name: " + this.name);
         System.out.println("Balance: " + this.balance);;
+    }
+
+    public void checkHistory () {
+        System.out.println("Checking History");
+        System.out.println(this.history);
     }
 }
--- 2. OOP/5. Projects/com.titya/src/2. Banking System/Main/src/Main.java
+++ 2. OOP/5. Projects/com.titya/src/2. Banking System/Main/src/Main.java
@@ -3,9 +3,9 @@ public class Main {
         var account = new BankAccount("Titya", 100);
 
         account.deposit(500);
-
         account.withdraw(200);
 
         account.checkBalance();
+        account.checkHistory();
     }
 }
