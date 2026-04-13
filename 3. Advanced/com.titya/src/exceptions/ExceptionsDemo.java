9af2cbdad4a8214392294f74d4fa2da37aaeb09f
Titya Yem <thitya.yem.photo@gmail.com>
2026-04-13 09:36:03 -0400

    Lesson: the finally block

--- 3. Advanced/com.titya/src/exceptions/ExceptionsDemo.java
+++ 3. Advanced/com.titya/src/exceptions/ExceptionsDemo.java
@@ -1,22 +1,25 @@
 package exceptions;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
-    public static void show () {
+    public static void show () throws IOException {
+        FileReader reader = null;
+
         try {
-            var reader = new FileReader("file.txt");
+            reader = new FileReader("file.txt");
             var value = reader.read();
-            new SimpleDateFormat().parse("");
-        } catch (IOException | ParseException e) {
+        } catch (IOException e) {
             System.out.println("Could not read file");
         }
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        }
+        finally {
+            if (reader != null)
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
     }
 }
