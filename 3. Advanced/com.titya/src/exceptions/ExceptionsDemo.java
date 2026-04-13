c6c9d742c35e0b1b6cf4103ca4b0e1e040dafc5e
Titya Yem <thitya.yem.photo@gmail.com>
2026-04-13 09:27:39 -0400

    Lesson: Catching multiple types of exceptions

--- 3. Advanced/com.titya/src/exceptions/ExceptionsDemo.java
+++ 3. Advanced/com.titya/src/exceptions/ExceptionsDemo.java
@@ -3,14 +3,20 @@ package exceptions;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show () {
         try {
             var reader = new FileReader("file.txt");
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            var value = reader.read();
+            new SimpleDateFormat().parse("");
+        } catch (IOException | ParseException e) {
+            System.out.println("Could not read file");
         }
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
     }
 }
