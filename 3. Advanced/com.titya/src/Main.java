16de0475cff6a0e206c9863f38bc68a145d98051
Titya Yem <thitya.yem.photo@gmail.com>
2026-04-16 14:36:53 -0400

    Lesson: Generic Classes and Inheritance

--- 3. Advanced/com.titya/src/Main.java
+++ 3. Advanced/com.titya/src/Main.java
@@ -1,10 +1,12 @@
 import generics.GenericList;
+import generics.Instructor;
 import generics.User;
 import generics.Utils;
 
 public class Main {
     public static void main(String[] args) {
-        Utils.max(1, 2);
-        Utils.print(1, "Lamdouy");
+        var instructors = new GenericList<Instructor>();
+        var users = new GenericList<User>();
+        Utils.printUsers(users);
     }
 }
---
+++ 3. Advanced/com.titya/src/generics/Instructor.java
@@ -0,0 +1,8 @@
+package generics;
+
+public class Instructor extends User {
+
+    public Instructor(int points) {
+        super(points);
+    }
+}
