dfcb6ee4663f5cd26720833e28cd5004413f606e
Titya Yem <thitya.yem.photo@gmail.com>
2026-07-29 13:41:14 -0400

    Generic: Multiple Type Parameters

--- 3. Advanced/com.titya/src/Main.java
+++ 3. Advanced/com.titya/src/Main.java
@@ -3,6 +3,5 @@ import generics.User;
 import generics.Utils;
 
 public static void main (String[] args) {
-    var max = Utils.max(new User(10),new User(20));
-    System.out.println(max);
+    Utils.print(1, 10);
 }
