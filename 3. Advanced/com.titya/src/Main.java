4e7911e792f7fb24844159b6c17ffbcf2a2bb0c1
Titya Yem <thitya.yem.photo@gmail.com>
2026-07-29 13:23:56 -0400

    Generic: Comparable Interface

--- 3. Advanced/com.titya/src/Main.java
+++ 3. Advanced/com.titya/src/Main.java
@@ -2,5 +2,13 @@ import generics.GenericList;
 import generics.User;
 
 public static void main (String[] args) {
-    var list = new GenericList<>();
+    var user1 = new User(10);
+    var user2 = new User(20);
+
+    if (user1.compareTo(user2) < 0)
+        System.out.println("User 1 < User 2");
+    else if (user1.compareTo(user2) > 0)
+        System.out.println("User 1 > User 2");
+    else
+        System.out.println("User 1 == User 2");
 }
--- 3. Advanced/com.titya/src/generics/User.java
+++ 3. Advanced/com.titya/src/generics/User.java
@@ -1,5 +1,17 @@
 package generics;
 
-public class User implements Comparable {
-    // just examples
+public class User implements Comparable<User> {
+    private int points;
+
+    public User(int point) {
+        this.points = point;
+    }
+
+    @Override
+    public int compareTo(User other) {
+        // this < other -> -1
+        // this == other -> 0
+        // this > other -> 1
+        return points - other.points;
+    }
 }
