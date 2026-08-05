4b471b08238aaf46b6eae2b684022348f329fd25
Titya Yem <thitya.yem.photo@gmail.com>
2026-08-05 12:42:15 -0400

    Collection: Iterable Interface

--- 3. Advanced/com.titya/src/Main.java
+++ 3. Advanced/com.titya/src/Main.java
@@ -1,10 +1,11 @@
 import generics.GenericList;
-import generics.Instructor;
-import generics.User;
-import generics.Utils;
 
 public static void main (String[] args) {
-    var instructors = new GenericList<Instructor>();
-    var users = new GenericList<User>();
-    Utils.printUsers(instructors);
+    var list = new GenericList<String>();
+    var iterator = list.iterator();
+
+    while (iterator.hasNext()) {
+        var current = iterator.next();
+        System.out.println(current);
+    }
 }
--- 3. Advanced/com.titya/src/generics/GenericList.java
+++ 3. Advanced/com.titya/src/generics/GenericList.java
@@ -1,6 +1,8 @@
 package generics;
 
-public class GenericList <T> {
+import java.util.Iterator;
+
+public class GenericList <T> implements Iterable<T>{
     private T[] items = (T[]) new Object[10];
     private int count;
 
@@ -10,5 +12,10 @@ public class GenericList <T> {
 
     public T get (int index) {
         return items[index];
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
     }
 }
