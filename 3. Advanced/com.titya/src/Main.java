495b7e90a9e15c5a18f9ccf7a13cc7c2fd93b12f
Titya Yem <thitya.yem.photo@gmail.com>
2026-08-09 14:05:26 -0400

    Collection: Comparator Interface

--- 3. Advanced/com.titya/src/Main.java
+++ 3. Advanced/com.titya/src/Main.java
@@ -1,14 +1,15 @@
 import collections.Customer;
+import collections.EmailComparator;
+
 import java.util.List;
 
 public static void main (String[] args) {
     List<Customer> customers = new ArrayList<>();
-    customers.add(new Customer("b"));
-    customers.add(new Customer("a"));
-    customers.add(new Customer("c"));
+    customers.add(new Customer("b", "e3"));
+    customers.add(new Customer("a", "e2"));
+    customers.add(new Customer("c", "e1"));
 
-    // in order to sort it we need to compare interface first
-    // the method is in Customer class named ComparedTo
-    Collections.sort(customers);
+//    Collections.sort(customers, new EmailComparator()); or using
+    customers.sort(new EmailComparator());
     System.out.println(customers);
 }
